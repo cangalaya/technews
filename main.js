cbaf64fd646d944698943bf4218d8e6e6bc426e8
alonso cangalaya <cangalaya.2007@gmail.com>
2022-02-18 01:56:14 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+document.querySelector(".btn-menu").addEventListener('click', () => {       // seleccionamos el elemento
+    document.querySelector(".nav-menu").classList.toggle("show");   //añadimos la clase en el elemento o la quitamos si la tiene con toggle
+})
